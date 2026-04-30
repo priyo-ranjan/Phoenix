0bf9c11267d0b9786274c9e0e54b36702387ef69
priyo-ranjan <priyoranjan69420@gmail.com>
2026-04-30 22:45:58 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,8 +7,42 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        print(f"{member} has joined the server.")
         channel = self.bot.get_channel(1499402322680348797)
-        await channel.send(f"Welcome to the server, {member.mention}! We're glad to have you here.")
+        print(channel)
+
+        if channel:
+
+            embed = discord.Embed(
+              title="🌌 Another story begins!",
+              description=f"Welcome {member.mention} to **{member.guild.name}**",
+              color=0x5865F2)
+
+            embed.set_thumbnail(url=member.display_avatar.url)
+
+            embed.set_image(
+              url="https://cdn.discordapp.com/attachments/1499252105947386088/1499458858643357807/LmpwZw.png?ex=69f4df62&is=69f38de2&hm=db0102eaa0c2ad5d06cb772f1fb83496af7585ce09e8192d0ae6f42d50d37caa&")
+
+            embed.add_field(
+              name="👤 Username",
+              value=member.name,
+              inline=True)
+
+            embed.add_field(
+              name="📈 Member Count",
+              value=member.guild.member_count,
+              inline=True)
+
+            embed.add_field(
+              name="🕒 Account Created",
+              value=member.created_at.strftime("%d %B %Y"),
+              inline=False)
+
+            embed.set_footer(
+              text="Enjoy your stay ✨")
+
+            await channel.send(embed=embed)
+
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
@@ -20,10 +54,10 @@ class Welcome(commands.Cog):
         
            embed = discord.Embed(
               title="💔 Member Left",
-              description=f"### {member.mention} left the server.",
+              description=f"### {member.mention} disappeared into the void...",
               color=0x1e1f22)
 
-           embed.set_thumbnail(url=member.avatar.url)
+           embed.set_thumbnail(url=member.display_avatar.url)
 
            embed.set_image(url="https://cdn.discordapp.com/attachments/1499252105947386088/1499453612777279710/sad-anime-blue-monday-illustration_23-2151910258.png?ex=69f4da7f&is=69f388ff&hm=d6c57d4c65be4cc0422193b5a551ebb20e3c1664c0415e9dd093d28cac8f486d&")
 
