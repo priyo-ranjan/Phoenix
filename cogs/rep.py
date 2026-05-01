d1edf5fe8f489f952c306486f6ae2f6e5af4ae56
priyo-ranjan <priyoranjan69420@gmail.com>
2026-05-01 20:52:43 +0530

    Update rep.py

--- cogs/rep.py
+++ cogs/rep.py
@@ -1,53 +1,70 @@
 import discord
 from discord.ext import commands
 import json
-class Rep(commands.Cog):
+import os
+
+if os.path.exists("/data"):
+    PATH = "/data/rep.json"
+else:
+    PATH = "rep.json"
+
+class Reputation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.cooldown(1, 86400, commands.BucketType.user)
-    @commands.command()
-    async def rep(self, ctx, member: discord.Member):
+    def get_data(self):
+        try:
+            if not os.path.exists(PATH):
+                return {}
+            with open(PATH, "r") as f:
+                return json.load(f)
+        except:
+            return {}
 
-        if member == ctx.author:
-            await ctx.send("You cannot give reputation to yourself 💀")
+    def save_data(self, data):
+        with open(PATH, "w") as f:
+            json.dump(data, f, indent=4)
+
+    @commands.command(name="rep")
+    async def rep(self, ctx, member: discord.Member):
+        if member.id == ctx.author.id:
+            await ctx.send("❌ You cannot give reputation to yourself!")
             return
-        with open("rep.json", "r") as f:
-            users = json.load(f)
+
+        if member.bot:
+            await ctx.send("❌ You cannot give reputation to a bot!")
+            return
+
+        data = self.get_data()
         user_id = str(member.id)
 
-        if user_id not in users:
-            users[user_id] = {
-                "rep":0
-            }
-
-        users[user_id]["rep"] += 1
-
-        with open("rep.json", "w") as f:
-            json.dump(users, f, indent = 4)
+        if user_id not in data:
+            data[user_id] = 0
+        
+        data[user_id] += 1
+        self.save_data(data)
 
         embed = discord.Embed(
-            title = "Reputation Given!",
-            description = (f"{ctx.author.mention} has given a reputation point to" f"{member.mention}!"),
-            color = 0x5865F2
+            description=f"⭐ {ctx.author.mention} has given a reputation point to {member.mention}!",
+            color=0xFFD700
         )
-        embed.set_thumbnail(url = member.avatar.url)
-        embed.add_field(
-            name = "Total Reputation",
-            value = users[user_id]["rep"]
-        )
-        await ctx.send(embed = embed)
-    @rep.error
-    async def rep_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            hours = round(error.retry_after / 3600, 1)
-            embed = discord.Embed(
-                description = (
-                    f"You can give reputation again in " f"{hours} hours!"
-                ),
-                color = 0xff0000
-            )
-            await ctx.send(embed = embed)
+        await ctx.send(embed=embed)
+
+    @commands.command(name="reps")
+    async def reps(self, ctx, member: discord.Member = None):
+        member = member or ctx.author
+        data = self.get_data()
+        user_id = str(member.id)
+        
+        points = data.get(user_id, 0)
+
+        embed = discord.Embed(title="🏆 Reputation Stats", color=0xFFD700)
+        embed.set_thumbnail(url=member.display_avatar.url)
+        embed.add_field(name="User", value=member.mention, inline=True)
+        embed.add_field(name="Total Rep", value=f"**{points}** Points", inline=True)
+        embed.set_footer(text="Give rep to others using !rep @user")
+        
+        await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(Rep(bot))
+    await bot.add_cog(Reputation(bot))s
