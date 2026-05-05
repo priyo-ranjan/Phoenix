6b7bbbd80d37aab6bb424504b8b058a6a4d33380
priyo-ranjan <priyoranjan69420@gmail.com>
2026-05-05 20:06:22 +0530

    updated

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 import discord
-<<<<<<< Updated upstream
-=======
 import random
->>>>>>> Stashed changes
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -44,25 +41,3 @@ async def on_ready():
     print(f"{bot.user} is online!")
 
 bot.run(TOKEN)
-
-<<<<<<< Updated upstream
-=======
-@bot.command()
-async def ping(ctx):
-    await ctx.send("yo mf!")
-
-@bot.command()
-async def say(ctx, *, message):
-    await ctx.send(message)
-
-@bot.command()
-async def roast(ctx):
-    roasts = ["Bro fights mosquitoes and loses",
-     "You got 999 pings in real life",
-      "Even Windows Updates faster than your brain",
-      "NPC Behaviour detected"]
-    await ctx.send(random.choice(roasts))
-
-bot.run(TOKEN)
->>>>>>> Stashed changes
-
