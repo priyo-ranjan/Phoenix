770465b49facb66cdd4e14746e8df6fe6d1778b2
priyo-ranjan <priyoranjan69420@gmail.com>
2026-05-07 01:36:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def on_ready():
     await bot.load_extension("cogs.verify")
     await bot.load_extension("cogs.rules")
     await bot.load_extension("cogs.status")
-    await bot.load_extension("cogs.setupveriffy")
+    await bot.load_extension("cogs.setupverify")
 
     print(f"{bot.user} is online!")
 
