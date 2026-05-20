620dd18fc74c7a8c9a20a250bc040e31cbefea85
priyo-ranjan <priyoranjan69420@gmail.com>
2026-05-21 02:22:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import aiosqlite
 import os
-DB_NAME = os.getenv("DB_NAME", "database")
-
+DB_NAME = "database_current"
 async def setup_database():
     async with aiosqlite.connect(DB_NAME) as db:
         await db.execute("""CREATE TABLE IF NOT EXISTS levels(
