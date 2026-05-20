78d4beb001b8f61be0dc87599c62cb0ed84973e8
priyo-ranjan <priyoranjan69420@gmail.com>
2026-05-21 02:36:57 +0530

    f

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import aiosqlite
 import os
-DB_NAME = "database_backup"
+DB_NAME = "database.db"
 async def setup_database():
     async with aiosqlite.connect(DB_NAME) as db:
         await db.execute("""CREATE TABLE IF NOT EXISTS levels(
