265efd38fce553bc12edefa8f303bc42fd910e57
Priyo Ranjan <priyoranjan69420@gmail.com>
2026-08-25 16:05:49 +0530

    updated model from llama to gpt

--- cogs/ai.py
+++ cogs/ai.py
@@ -12,7 +12,7 @@ class AI(commands.Cog):
     @commands.command()
     async def ai(self, ctx, *, prompt):
         response = client.chat.completions.create(
-          model="llama-3.3-70b-versatile",
+          model="openai/gpt-oss-120b",
           messages=[
             {
                 "role": "system", 
