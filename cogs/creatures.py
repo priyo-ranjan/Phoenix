06265696f0348f575b60ce2a04dbac7eb88a3ce7
Priyo Ranjan <priyoranjan69420@gmail.com>
2026-09-04 18:08:30 +0530

    Create creatures.py
    added game creatures

---
+++ cogs/creatures.py
@@ -0,0 +1,119 @@
+CREATURES = {
+
+    "common": [
+        {"name": "Wolf", "rarity": "Common", "weight": 100},
+        {"name": "Fox", "rarity": "Common", "weight": 95},
+        {"name": "Bear", "rarity": "Common", "weight": 90},
+        {"name": "Lion", "rarity": "Common", "weight": 85},
+        {"name": "Python", "rarity": "Common", "weight": 80},
+        {"name": "Stag", "rarity": "Common", "weight": 75},
+        {"name": "Boar", "rarity": "Common", "weight": 70},
+
+        {"name": "Tiger", "rarity": "Uncommon", "weight": 87},
+        {"name": "Eagle", "rarity": "Uncommon", "weight": 83},
+        {"name": "Crocodile", "rarity": "Uncommon", "weight": 79},
+
+        {"name": "Dire Hound", "rarity": "Rare", "weight": 42},
+        {"name": "Moon Owl", "rarity": "Rare", "weight": 40},
+        {"name": "Crystal Stag", "rarity": "Rare", "weight": 38},
+
+        {"name": "Ember Drake", "rarity": "Very Rare", "weight": 30},
+    ],
+
+
+    "extinct": [
+        {"name": "Dodo", "rarity": "Common", "weight": 100},
+        {"name": "Woolly Mammoth", "rarity": "Common", "weight": 95},
+
+        {"name": "Smilodon", "rarity": "Uncommon", "weight": 68},
+        {"name": "Woolly Rhino", "rarity": "Uncommon", "weight": 65},
+        {"name": "Velociraptor", "rarity": "Uncommon", "weight": 62},
+
+        {"name": "Triceratops", "rarity": "Rare", "weight": 42},
+        {"name": "Spinosaurus", "rarity": "Rare", "weight": 40},
+        {"name": "Tyrannosaurus Rex", "rarity": "Rare", "weight": 38},
+
+        {"name": "Megalodon", "rarity": "Very Rare", "weight": 20},
+        {"name": "Deinosuchus", "rarity": "Very Rare", "weight": 19},
+        {"name": "Argentavis", "rarity": "Very Rare", "weight": 18},
+
+        {"name": "Titanoboa", "rarity": "Legendary", "weight": 10},
+        {"name": "Giganotosaurus", "rarity": "Legendary", "weight": 9},
+
+        {"name": "Fossil Wyrm", "rarity": "Mythic", "weight": 5},
+    ],
+
+
+    "dragon": [
+        {"name": "Ember Drake", "rarity": "Common", "weight": 100},
+        {"name": "Stone Drake", "rarity": "Common", "weight": 95},
+
+        {"name": "Frost Drake", "rarity": "Uncommon", "weight": 75},
+        {"name": "Venom Drake", "rarity": "Uncommon", "weight": 71},
+
+        {"name": "Wyvern", "rarity": "Rare", "weight": 37},
+        {"name": "Sea Serpent", "rarity": "Rare", "weight": 35},
+        {"name": "Thunder Dragon", "rarity": "Rare", "weight": 33},
+
+        {"name": "Ice Dragon", "rarity": "Very Rare", "weight": 30},
+        {"name": "Inferno Dragon", "rarity": "Very Rare", "weight": 28},
+
+        {"name": "Storm Wyrm", "rarity": "Legendary", "weight": 11},
+        {"name": "Ancient Dragon", "rarity": "Legendary", "weight": 10},
+        {"name": "Hydra", "rarity": "Legendary", "weight": 9},
+
+        {"name": "Elder Wyrm", "rarity": "Mythic", "weight": 4},
+        {"name": "World Serpent", "rarity": "Mythic", "weight": 3},
+    ],
+
+
+    "mythic": [
+        {"name": "Kitsune", "rarity": "Common", "weight": 100},
+        {"name": "Basilisk", "rarity": "Common", "weight": 95},
+
+        {"name": "Griffin", "rarity": "Uncommon", "weight": 84},
+        {"name": "Naga", "rarity": "Uncommon", "weight": 80},
+
+        {"name": "Fenrir", "rarity": "Rare", "weight": 36},
+        {"name": "Cerberus", "rarity": "Rare", "weight": 34},
+        {"name": "Roc", "rarity": "Rare", "weight": 32},
+        {"name": "Kelpie", "rarity": "Rare", "weight": 30},
+
+        {"name": "Unicorn", "rarity": "Rare", "weight": 31},
+        {"name": "Hippogriff", "rarity": "Rare", "weight": 29},
+
+        {"name": "Jörmungandr", "rarity": "Very Rare", "weight": 30},
+        {"name": "Thunderbird", "rarity": "Very Rare", "weight": 28},
+        {"name": "Manticore", "rarity": "Very Rare", "weight": 26},
+
+        {"name": "Quetzalcoatl", "rarity": "Legendary", "weight": 25},
+        {"name": "Phoenix", "rarity": "Legendary", "weight": 24},
+
+        {"name": "Leviathan", "rarity": "Mythic", "weight": 13},
+        {"name": "Amaterasu's Wolf", "rarity": "Mythic", "weight": 12},
+    ],
+
+
+    "cosmic": [
+        {"name": "Lunar Wolf", "rarity": "Common", "weight": 100},
+        {"name": "Comet Serpent", "rarity": "Common", "weight": 95},
+
+        {"name": "Void Raven", "rarity": "Uncommon", "weight": 88},
+        {"name": "Stellar Stag", "rarity": "Uncommon", "weight": 84},
+
+        {"name": "Eclipse Panther", "rarity": "Rare", "weight": 55},
+        {"name": "Solar Lion", "rarity": "Rare", "weight": 52},
+        {"name": "Planetary Leviathan", "rarity": "Rare", "weight": 49},
+
+        {"name": "Nebula Dragon", "rarity": "Very Rare", "weight": 56},
+        {"name": "Void Hydra", "rarity": "Very Rare", "weight": 53},
+
+        {"name": "Celestial Phoenix", "rarity": "Legendary", "weight": 33},
+        {"name": "Starforged Dragon", "rarity": "Legendary", "weight": 31},
+        {"name": "Eclipse Sovereign", "rarity": "Legendary", "weight": 29},
+
+        {"name": "Voidwalker", "rarity": "Mythic", "weight": 19},
+        {"name": "The Observer", "rarity": "Mythic", "weight": 18},
+        {"name": "Cosmic Devourer", "rarity": "Mythic", "weight": 17},
+    ]
+}
